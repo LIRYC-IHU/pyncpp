696d2762cfe5b091feaf3c1f97904a9c7be93241
Florent Collot <florent.collot@ihu-liryc.fr>
2022-12-09 15:28:40 +0100

    fix packaging of Python

--- cmake/add_path_config_file.cmake
+++ cmake/add_path_config_file.cmake
@@ -1,48 +1,66 @@
 # Copyright (c) 2022 IHU Liryc, Université de Bordeaux, Inria.
 # License: BSD-3-Clause
 
-function(PYNCPP_add_path_config_file executable)
+function(PYNCPP_add_path_config_file executable_path)
 
     cmake_parse_arguments(PARSE_ARGV 1 "ARG"
-        ""
-        ""
-        "PATHS"
+        "ABSOLUTE_PATHS"
+        "STDLIB"
+        "PATHS;EXTRA_PATHS"
         )
 
-    if(NOT ARG_PATHS)
-        message(FATAL_ERROR "Missing PATHS.")
-    endif()
-
-    if(TARGET ${executable})
-        get_target_property(target_type ${executable} TYPE)
-        if(NOT target_type STREQUAL "EXECUTABLE")
-            message(FATAL_ERROR "Target must be an executable.")
-        endif()
-
-        string(REPLACE "\;" "\\\\;" paths "${ARG_PATHS}")
-
-        add_custom_command(TARGET ${executable} POST_BUILD
-            COMMAND ${CMAKE_COMMAND}
-            -D "EXECUTABLE:PATH=$<TARGET_FILE_DIR:${executable}>/$<TARGET_FILE_NAME:${executable}>"
-            -D "PATHS:STRING=${paths}"
-            -P "${CMAKE_CURRENT_FUNCTION_LIST_FILE}"
-            VERBATIM
-            )
+    if(ARG_PATHS)
+        set(paths ${ARG_PATHS})
     else()
-        get_filename_component(executable_dir "${executable}" DIRECTORY)
-        get_filename_component(executable_name "${executable}" NAME_WLE)
-
-        set(path_config_file "${executable_dir}/${executable_name}._pth")
-        file(WRITE "${path_config_file}")
-
-        foreach(path ${ARG_PATHS})
-            file(RELATIVE_PATH path "${executable_dir}" "${path}")
-            file(APPEND "${path_config_file}" "${path}\n")
-        endforeach()
+        if(ARG_STDLIB)
+            set(paths
+                "${ARG_STDLIB}"
+                "${ARG_STDLIB}/lib-dynload"
+                )
+        else()
+            set(paths
+                "${Python_STDLIB}"
+                "${Python_STDARCH}"
+                )
+        endif()
+        if(ARG_EXTRA_PATHS)
+            list(APPEND paths ${ARG_EXTRA_PATHS})
+        endif()
     endif()
+
+    get_filename_component(executable_dir "${executable_path}" DIRECTORY)
+    get_filename_component(executable_name "${executable_path}" NAME_WLE)
+
+    set(path_config_file "${executable_dir}/${executable_name}._pth")
+    file(WRITE "${path_config_file}")
+
+    foreach(path ${paths})
+        if(ARG_ABSOLUTE_PATHS)
+            if(NOT IS_ABSOLUTE "${path}")
+                message(FATAL_ERROR "Path is not absolute: ${path}")
+            endif()
+        else()
+            if(IS_ABSOLUTE "${path}")
+                file(RELATIVE_PATH path "${executable_dir}" "${path}")
+            endif()
+        endif()
+        file(APPEND "${path_config_file}" "${path}\n")
+    endforeach()
 
 endfunction()
 
 if(CMAKE_SCRIPT_MODE_FILE AND ((NOT CMAKE_PARENT_LIST_FILE) OR (CMAKE_PARENT_LIST_FILE STREQUAL CMAKE_CURRENT_LIST_FILE)))
-    PYNCPP_add_path_config_file(${EXECUTABLE} PATHS ${PATHS})
+
+    set(arguments ${EXECUTABLE})
+
+    if(ABSOLUTE_PATHS)
+        list(APPEND arguments ABSOLUTE_PATHS)
+    endif()
+
+    if(PATHS)
+        list(APPEND arguments PATHS ${PATHS})
+    endif()
+
+    PYNCPP_add_path_config_file(${arguments})
+
 endif()
