cc723ee668087190820a86d5113b0646c848e219
Florent Collot <florent.collot@ihu-liryc.fr>
2023-04-26 11:51:04 +0200

    Lowercase project name consistency, windows ommissions

--- cmake/import_python_library_unix.cmake
+++ cmake/import_python_library_unix.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2023 IHU Liryc, Université de Bordeaux, Inria.
 # License: BSD-3-Clause
 
-macro(PYNCPP_import_python_library_unix target_name)
+macro(pyncpp_import_python_library_unix target_name)
 
     cmake_parse_arguments("ARG"
         ""
@@ -12,18 +12,18 @@ macro(PYNCPP_import_python_library_unix target_name)
 
     add_library(${target_name} SHARED IMPORTED GLOBAL)
 
-    set(lib_path "lib/libpython${PYNCPP_PYTHON_SHORT_VERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    set(include_path "include/python${PYNCPP_PYTHON_SHORT_VERSION}")
+    set(lib_path "lib/libpython${pyncpp_PYTHON_SHORT_VERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    set(include_path "include/python${pyncpp_PYTHON_SHORT_VERSION}")
 
     if(ARG_INSTALLED OR CMAKE_BUILD_TYPE)
-        set_target_properties(${target_name} PROPERTIES IMPORTED_LOCATION "${PYNCPP_PYTHON_DIR}/${lib_path}")
-        set(include_dir "${PYNCPP_PYTHON_DIR}/${include_path}")
+        set_target_properties(${target_name} PROPERTIES IMPORTED_LOCATION "${pyncpp_PYTHON_DIR}/${lib_path}")
+        set(include_dir "${pyncpp_PYTHON_DIR}/${include_path}")
     else()
         set(include_dir)
         foreach(config ${CMAKE_CONFIGURATION_TYPES})
             string(TOUPPER ${config} CONFIG)
-            set_target_properties(${target_name} PROPERTIES IMPORTED_LOCATION_${CONFIG} "${PYNCPP_PYTHON_DIR}/${config}/${lib_path}")
-            string(APPEND include_dir $<$<CONFIG:${config}>:${PYNCPP_PYTHON_DIR}/${config}/${include_path}>)
+            set_target_properties(${target_name} PROPERTIES IMPORTED_LOCATION_${CONFIG} "${pyncpp_PYTHON_DIR}/${config}/${lib_path}")
+            string(APPEND include_dir $<$<CONFIG:${config}>:${pyncpp_PYTHON_DIR}/${config}/${include_path}>)
         endforeach()
     endif()
 
